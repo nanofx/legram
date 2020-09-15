4f6d592cd2769ac6f09a807d2115fcf7b41b0665
nanofx <nanofx01@gmail.com>
2020-09-15 08:00:43 +0300

    Add files via upload

---
+++ build/precache-manifest.2ba5fd3ce76276a3fc5271b5f7f8bdb3.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "14a65c294c5b1cf42948ad7cfe614132",
+    "url": "/index.html"
+  },
+  {
+    "revision": "10ac9d6966aa85873cdd",
+    "url": "/static/css/2.764ccc25.chunk.css"
+  },
+  {
+    "revision": "10ac9d6966aa85873cdd",
+    "url": "/static/js/2.88b46295.chunk.js"
+  },
+  {
+    "revision": "52b2a909ca3cc978ab9c",
+    "url": "/static/js/main.752774a4.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "79b4d48d465a4ceb49e6747eb33a4404",
+    "url": "/static/media/background.79b4d48d.jpg"
+  },
+  {
+    "revision": "d60d852e9643287d4065be5e71c70b62",
+    "url": "/static/media/background.d60d852e.jpeg"
+  }
+]);
